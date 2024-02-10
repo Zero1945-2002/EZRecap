82db1ef62b5cc42b96747b5c6396892f01bb3457
Zaid Immaduddin A <109498708+zaid-2002@users.noreply.github.com>
2024-02-10 17:59:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ import os
 import subprocess
 import win32com.client
 
-
-
-TR = ['TR1', 'TR2', 'TR3', 'TR4', 'TR5', 'TR6', 'TR7', 'TR8', 'TR9', 'TR10']
-TA = ['TA1', 'TA2', 'TA3', 'TA4', 'TA5', 'TA6', 'TA7', 'TA8', 'TA9', 'TA10']
-
 file_types = [("Excel Workbook (*.xlsx)", "*.xlsx"),
               ("Excel 97-2003 Workbook (*.xls)", "*.xls"),
               ("All files (*.*)", "*.*")]
@@ -195,21 +190,8 @@ while True:
                 workbook = load_workbook(filename=nama_file)
                 sheet = workbook.sheetnames
                 col1 = sg.Column([
-                                [sg.Frame('Kelas:',[[sg.Text('Kelas:'),sg.Listbox(sheet, size=(20, 2), enable_events=True, key='-KELAS-')],],size=(505,70))],
-                                [sg.Frame('NIM:',[[sg.Text('NIM:'),sg.Input(tooltip='ex : 202111129', default_text='', key='-NIM-', size=(19,1))]],size=(505,50))]], size=(515,140), pad=(0,0))                                  
-                col2 = sg.Column([[sg.Frame('Tugas Rumah:', [[sg.Text(), sg.Column([
-                                                                                    [sg.Listbox(TR, size=(20, 5), enable_events=True, key='-TR-')],
-                                                                                    [sg.Text('Nilai:'),sg.Input(tooltip='masukkan nilai 1 s/d 100', default_text='', key='-NILAI-TR-', size=(19,1))],
-                                                                                    [sg.Button('Simpan Nilai TR')]], size=(230,155), pad=(0,0))]])] ], pad=(0,0))
-                col3 = sg.Column([[sg.Frame('Test Awal:', [[sg.Text(), sg.Column([
-                                                                                [sg.Listbox(TA, size=(20, 5), enable_events=True, key='-TA-')],
-                                                                                [sg.Text('Nilai:'),sg.Input(tooltip='masukkan nilai 1 s/d 100', default_text='', key='-NILAI-TA-', size=(19,1))],
-                                                                                [sg.Button('Simpan Nilai TA')]], size=(230,155), pad=(0,0))]])] ], pad=(0,0))
-                col4 = sg.Column([[sg.Frame('Actions:',
-                                                    [[sg.Column([[sg.Button('Kembali'), sg.Button('Buka File')]],size=(460,45), pad=(0,0))]],size=(505,60))]], pad=(0,0))
-                halaman_lanjut = [[col1],
-                        [col2, col3],
-                        [col4]]
+                                [sg.Frame('Kelas:',[[sg.Text('Kelas:'),sg.Listbox(sheet, size=(20, 2), enable_events=True, key='-KELAS-'), sg.Text('    *klik di sini untuk memilih kelas', text_color="#3e3e3e")],],size=(505,70))]], size=(515,75), pad=(0,0))                                  
+                halaman_lanjut = [[col1]]
                 
                 window.close()
                 window = sg.Window('Rekap Nilai', halaman_lanjut)
@@ -240,6 +222,44 @@ while True:
             print(f"File tidak ditemukan")
         except Exception as e:
             print("Terjadi kesalahan:", str(e))
+    elif event == '-KELAS-': 
+        kelas = str(values['-KELAS-'][0])
+        ws = workbook[kelas]
+        cek_tr = 0
+        cek_ta = 0
+        TR = []
+        TA = []
+        for k in ws.iter_cols():
+            for c in k:
+                teks = str(c.value)
+                if teks[0].upper() == "T" and teks[1].upper() == "R":
+                    TR.append(c.value)
+                    cek_tr = cek_tr + 1
+                if teks[0].upper() == "T" and teks[1].upper() == "A":
+                    TA.append(c.value)
+                    cek_ta = cek_ta + 1
+        is_file_open(nama_file)
+        workbook = load_workbook(filename=nama_file)
+        sheet = workbook.sheetnames
+        col1 = sg.Column([
+                        [sg.Frame('Kelas:',[[sg.Text('Kelas:'),sg.Listbox(sheet,default_values=kelas, size=(20, 2), enable_events=True, key='-KELAS-'), sg.Text('    *klik di sini untuk mengganti kelas', text_color="#3e3e3e")],],size=(505,70))],
+                        [sg.Frame('NIM:',[[sg.Text('NIM:'),sg.Input(tooltip='ex : 202111129', default_text='', key='-NIM-', size=(19,1))]],size=(505,50))]], size=(515,140), pad=(0,0))                                  
+        col2 = sg.Column([[sg.Frame('Tugas Rumah:', [[sg.Text(), sg.Column([
+                                                                            [sg.Listbox(TR, size=(20, 5), enable_events=True, key='-TR-')],
+                                                                            [sg.Text('Nilai:'),sg.Input(tooltip='masukkan nilai 1 s/d 100', default_text='', key='-NILAI-TR-', size=(19,1))],
+                                                                            [sg.Button('Simpan Nilai TR')]], size=(230,155), pad=(0,0))]])] ], pad=(0,0))
+        col3 = sg.Column([[sg.Frame('Test Awal:', [[sg.Text(), sg.Column([
+                                                                        [sg.Listbox(TA, size=(20, 5), enable_events=True, key='-TA-')],
+                                                                        [sg.Text('Nilai:'),sg.Input(tooltip='masukkan nilai 1 s/d 100', default_text='', key='-NILAI-TA-', size=(19,1))],
+                                                                        [sg.Button('Simpan Nilai TA')]], size=(230,155), pad=(0,0))]])] ], pad=(0,0))
+        col4 = sg.Column([[sg.Frame('Actions:',
+                                            [[sg.Column([[sg.Button('Kembali'), sg.Button('Buka File')]],size=(460,45), pad=(0,0))]],size=(505,60))]], pad=(0,0))
+        halaman_lanjut = [[col1],
+                [col2, col3],
+                [col4]]
+        
+        window.close()
+        window = sg.Window('Rekap Nilai Kelas '+kelas, halaman_lanjut)
 
 
 window.close()
