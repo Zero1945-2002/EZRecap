b63812169b64d0507de6bfcdd503a661021d53ce
Zaid Immaduddin A <109498708+zaid-2002@users.noreply.github.com>
2024-02-11 23:16:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,57 @@ from openpyxl import load_workbook
 import os
 import subprocess
 
-file_types = [("Excel Workbook (*.xlsx)", "*.xlsx"),
-              ("Excel 97-2003 Workbook (*.xls)", "*.xls"),
-              ("All files (*.*)", "*.*")]
+file_types = [
+    ("Excel Workbook (*.xlsx)", "*.xlsx"),  # Format file Excel xlsx
+    ("Excel 97-2003 Workbook (*.xls)", "*.xls"),  # Format file Excel xls (versi lama)
+    ("All files (*.*)", "*.*"),  # Semua jenis file
+    ("Excel Macro-Enabled Workbook (*.xlsm)", "*.xlsm"),  # Format file Excel xlsm (mengandung macro)
+    ("Excel Template (*.xltx)", "*.xltx"),  # Format file Excel xltx (template)
+    ("Excel Macro-Enabled Template (*.xltm)", "*.xltm"),  # Format file Excel xltm (template mengandung macro)
+    ("CSV (Comma Delimited) (*.csv)", "*.csv"),  # Format file Excel csv (Comma Separated Values)
+    ("Text File (*.txt)", "*.txt"),  # Format file Excel txt (Text File)
+    ("PRN (Printable) (*.prn)", "*.prn")  # Format file Excel prn (Printable)
+]
 
-halaman_awal = [[
-            sg.Text("Excel File"),
-            sg.Input(tooltip='klik tombol browse untuk memilih file',default_text='',size=(25, 1), key="-FILE-"),
-            sg.FileBrowse(file_types=file_types),
-            sg.Button("Confirm")
-        ]]
 
-window = sg.Window("Rekap Nilai", halaman_awal)
+sheet = []
+TR = []
+TA = []
+
+def popup():
+    layout = [[
+                sg.Text("Excel File"),
+                sg.Input(tooltip='klik tombol browse untuk memilih file',size=(25, 1), key="-FILE-"),
+                sg.FileBrowse(file_types=file_types),
+                sg.Button("Confirm")
+            ]]
+    event, values = sg.Window('Pilih File', layout, modal=True).read(close=True)
+    return None if event == sg.WIN_CLOSED else str(values['-FILE-'])
+
+menu_def = [['&File', ['&Pilih File','&Keluar']],
+            ['&Help', ['&Cara Penggunaan','&About']] ]
+
+col1 = sg.Column([
+                        [sg.Frame('Kelas:',[[sg.Text('Kelas:'),sg.Combo(sheet, size=(20, 10), enable_events=True, key='-KELAS-', readonly=True), sg.Text('    *klik di sini untuk mengganti kelas', text_color="#3e3e3e")],],size=(505,60))],
+                        [sg.Frame('NIM:',[[sg.Text('NIM:'),sg.Input(tooltip='ex : 202111129', default_text='', key='-NIM-', size=(19,1))]],size=(505,50))]], size=(515,130), pad=(0,0))                                  
+col2 = sg.Column([[sg.Frame('Tugas Rumah:', [[sg.Text(), sg.Column([
+                                                                    [sg.Combo(TR, tooltip='klik disini untuk memilih jenis TR', size=(20, 10), enable_events=True, key='-TR-', readonly=True)],
+                                                                    [sg.Text('Nilai:'),sg.Input(tooltip='masukkan nilai 1 s/d 100', default_text='', key='-NILAI-TR-', size=(19,1))],
+                                                                    [sg.Button('Simpan Nilai TR')]], size=(230,105), pad=(0,0))]])] ], pad=(0,0))
+col3 = sg.Column([[sg.Frame('Test Awal:', [[sg.Text(), sg.Column([
+                                                                [sg.Combo(TA, tooltip='klik disini untuk memilih jenis TA', size=(20, 10), enable_events=True, key='-TA-', readonly=True)],
+                                                                [sg.Text('Nilai:'),sg.Input(tooltip='masukkan nilai 1 s/d 100', default_text='', key='-NILAI-TA-', size=(19,1))],
+                                                                [sg.Button('Simpan Nilai TA')]], size=(230,105), pad=(0,0))]])] ], pad=(0,0))
+col4 = sg.Column([[sg.Frame('Actions:',[[sg.Column([[sg.Button('Buka File')]],size=(460,45), pad=(0,0))]],size=(505,60))]], pad=(0,0))
+
+layout = [ [sg.MenubarCustom(menu_def, key='-MENU-', tearoff=False)]]  
+
+layout += [[col1],
+        [col2, col3],
+        [col4]]
+
+window = sg.Window('EZRecap', layout, margins=(0,0))
+
 
 def cek(var,text):
     if var == False:
@@ -22,11 +61,17 @@ def cek(var,text):
         return False
     return True
 
-def update_nilai_tr():
+def update_nilai(type):
+    if type == 'TR':
+        t_type = "-TR-"
+        n_type = "-NILAI-TR-"
+    elif type == 'TA':
+        t_type = "-TA-"
+        n_type = "-NILAI-TA-"
     nama = False
     nim = False
     p_nim = False
-    jenis_tr = False
+    jenis = False
 
     # Kelas
     if len(values['-KELAS-']) == 0:
@@ -38,14 +83,14 @@ def update_nilai_tr():
         sg.popup('Masukkan NIM terlebih dahulu!')
         return
     
-    # Jenis TR
-    if len(values['-TR-']) == 0:
-        sg.popup('Mohon pilih TR terlebih dahulu!')
+    # Jenis T
+    if len(values[t_type]) == 0:
+        sg.popup('Mohon pilih jenis tugas '+t_type+' terlebih dahulu!')
         return
 
     # Nilai TR
-    if str(values['-NILAI-TR-']).isdigit():
-        nilai = int(values['-NILAI-TR-'])
+    if str(values[n_type]).isdigit():
+        nilai = int(values[n_type])
         if nilai < 0 or nilai > 100:
             sg.popup('Masukkan nilai dengan range 0 s/d 100')
             return
@@ -66,9 +111,9 @@ def update_nilai_tr():
                         if str(cell.value) == str(values['-NIM-']):
                             nim_row = cell.row
                             p_nim = True
-            if c.value == values['-TR-'][0]:
-                tr_col = c.column
-                jenis_tr = True
+            if c.value == values[t_type]:
+                t_col = c.column
+                jenis = True
             if c.value == "Nama" or c.value == "NAMA" or c.value == "nama":
                 nama_col = c.column
                 nama = True
@@ -76,13 +121,13 @@ def update_nilai_tr():
         return
     if cek(p_nim,'Praktikan dengan nim '+str(values['-NIM-'])+' tidak ditemukan!') == False:
         return
-    if cek(jenis_tr,"Pastikan kolom '"+str(values['-TR-'][0])+"' tersedia!") == False:
+    if cek(jenis,"Pastikan kolom '"+str(values[t_type])+"' tersedia!") == False:
         return
     if cek(nama,"Pastikan kolom 'Nama' tersedia!") == False:
         return
 
     # Ubah nilai cell
-    ws.cell(row=nim_row, column=tr_col, value=int(values['-NILAI-TR-']))
+    ws.cell(row=nim_row, column=t_col, value=int(values[n_type]))
     try:
         workbook.save(filename=filename)
     except PermissionError:
@@ -90,117 +135,42 @@ def update_nilai_tr():
         return
     nama_praktikan = ws.cell(row=nim_row, column=nama_col).value
     nim_praktikan = ws.cell(row=nim_row, column=nim_col).value
-    teks = 'Nilai '+str(values['-TR-'][0])+' praktikan '+str(nama_praktikan)+' ('+str(nim_praktikan)+') telah di update!'
+    teks = 'Nilai '+str(values[t_type])+' praktikan '+str(nama_praktikan)+' ('+str(nim_praktikan)+') berhasil dimasukkan!'
     sg.popup(teks)
     return
-
-def update_nilai_ta():
-    nama = False
-    nim = False
-    p_nim = False
-    jenis_ta = False
-
-    # Kelas
-    if len(values['-KELAS-']) == 0:
-        sg.popup('Mohon pilih kelasnya terlebih dahulu!')
-        return
-
-    # NIM
-    if str(values['-NIM-']) == ' ' or str(values['-NIM-']) == '':
-        sg.popup('Masukkan NIM terlebih dahulu!')
-        return
-    
-    # Jenis TR
-    if len(values['-TA-']) == 0:
-        sg.popup('Mohon pilih TA terlebih dahulu!')
-        return
-
-    # Nilai TR
-    if str(values['-NILAI-TA-']).isdigit():
-        nilai = int(values['-NILAI-TA-'])
-        if nilai < 0 or nilai > 100:
-            sg.popup('Masukkan nilai dengan range 0 s/d 100')
-            return
-    else:
-        sg.popup('Masukkan nilai dengan range 0 s/d 100')
-        return
-
-    # Pencarian cell
-    kelas = str(values['-KELAS-'][0])
-    ws = workbook[kelas]
-    for k in ws.iter_cols():
-        for c in k:
-            if c.value == "NIM" or c.value == "nim" or c.value == "Nim":
-                nim_col = c.column
-                nim = True
-                for row in ws.iter_rows(min_col=nim_col, max_col=nim_col):
-                    for cell in row:
-                        if str(cell.value) == str(values['-NIM-']):
-                            nim_row = cell.row
-                            p_nim = True
-            if c.value == values['-TA-'][0]:
-                ta_col = c.column
-                jenis_ta = True
-            if c.value == "Nama" or c.value == "NAMA" or c.value == "nama":
-                nama_col = c.column
-                nama = True
-    if cek(nim,"Pastikan kolom 'NIM' tersedia!") == False:
-        return
-    if cek(p_nim,'Praktikan dengan nim '+str(values['-NIM-'])+' tidak ditemukan!') == False:
-        return
-    if cek(jenis_ta,"Pastikan kolom '"+str(values['-TA-'][0])+"' tersedia!") == False:
-        return
-    if cek(nama,"Pastikan kolom 'Nama' tersedia!") == False:
-        return
-
-    # Ubah nilai cell
-    ws.cell(row=nim_row, column=ta_col, value=int(values['-NILAI-TA-']))
-    try:
-        workbook.save(filename=filename)
-    except PermissionError:
-        sg.popup('Harap tutup terlebih dahulu file yang terbuka!')
-        return
-    nama_praktikan = ws.cell(row=nim_row, column=nama_col).value
-    nim_praktikan = ws.cell(row=nim_row, column=nim_col).value
-    teks = 'Nilai '+str(values['-TA-'][0])+' praktikan '+str(nama_praktikan)+' ('+str(nim_praktikan)+') telah di update!'
-    sg.popup(teks)
-    return
-            
+   
 while True:
     event, values = window.read()
     print(event, values)
-    if event == sg.WIN_CLOSED:
+    if event == sg.WIN_CLOSED or event == "Keluar":
         break
-    elif event == "Confirm":
-        if values['-FILE-'] == ' ' or values['-FILE-'] == '':
+
+    elif event == "Pilih File":
+        filename = popup()
+        if filename != None:
+            window.write_event_value('proses file', None)
+
+    elif event == "proses file":
+        if filename == ' ' or filename == '':
             sg.popup("Klik tombol 'Browse' untuk memilih file!")
+            window.write_event_value("Pilih File", None)
         else:
-            filename = values["-FILE-"]
             if os.path.isfile(filename):
+                sg.popup('File berhasil diakses!') 
                 nama_file = filename
                 workbook = load_workbook(filename=nama_file)
                 sheet = workbook.sheetnames
-                col1 = sg.Column([
-                                [sg.Frame('Kelas:',[[sg.Text('Kelas:'),sg.Listbox(sheet, size=(20, 2), enable_events=True, key='-KELAS-'), sg.Text('    *klik di sini untuk memilih kelas', text_color="#3e3e3e")],],size=(505,70))]], size=(515,75), pad=(0,0))                                  
-                halaman_lanjut = [[col1]]
-                
-                window.close()
-                window = sg.Window('Rekap Nilai', halaman_lanjut)
+                window['-KELAS-'].update(values=sheet)
             else:
                 sg.popup('File tidak ditemukan!')
+                window.write_event_value("Pilih File", None)
+
     elif event == 'Simpan Nilai TR':
-        update_nilai_tr()
+        update_nilai("TR")
+
     elif event == 'Simpan Nilai TA':
-        update_nilai_ta()
-    elif event == 'Kembali':
-        halaman_awal = [[
-            sg.Text("Excel File"),
-            sg.Input(tooltip='klik tombol browse untuk memilih file',default_text='',size=(25, 1), key="-FILE-"),
-            sg.FileBrowse(file_types=file_types),
-            sg.Button("Confirm")
-        ]]
-        window.close()
-        window = sg.Window('Rekap Nilai', halaman_awal)
+        update_nilai("TA")
+
     elif event == 'Buka File':
         try:
             # Membuka file Excel dengan aplikasi yang terkait
@@ -218,7 +188,8 @@ while True:
         except FileNotFoundError:
             sg.popup(f"File tidak ditemukan")
         except Exception as e:
-            sg.popup("Terjadi kesalahan:", str(e))
+            sg.popup("Mohon pilih filenya terlebih dahulu!")
+
     elif event == '-KELAS-': 
         kelas = str(values['-KELAS-'][0])
         ws = workbook[kelas]
@@ -235,27 +206,29 @@ while True:
                 if teks[0].upper() == "T" and teks[1].upper() == "A":
                     TA.append(c.value)
                     cek_ta = cek_ta + 1
-        workbook = load_workbook(filename=nama_file)
-        sheet = workbook.sheetnames
-        col1 = sg.Column([
-                        [sg.Frame('Kelas:',[[sg.Text('Kelas:'),sg.Listbox(sheet,default_values=kelas, size=(20, 2), enable_events=True, key='-KELAS-'), sg.Text('    *klik di sini untuk mengganti kelas', text_color="#3e3e3e")],],size=(505,70))],
-                        [sg.Frame('NIM:',[[sg.Text('NIM:'),sg.Input(tooltip='ex : 202111129', default_text='', key='-NIM-', size=(19,1))]],size=(505,50))]], size=(515,140), pad=(0,0))                                  
-        col2 = sg.Column([[sg.Frame('Tugas Rumah:', [[sg.Text(), sg.Column([
-                                                                            [sg.Listbox(TR, size=(20, 5), enable_events=True, key='-TR-')],
-                                                                            [sg.Text('Nilai:'),sg.Input(tooltip='masukkan nilai 1 s/d 100', default_text='', key='-NILAI-TR-', size=(19,1))],
-                                                                            [sg.Button('Simpan Nilai TR')]], size=(230,155), pad=(0,0))]])] ], pad=(0,0))
-        col3 = sg.Column([[sg.Frame('Test Awal:', [[sg.Text(), sg.Column([
-                                                                        [sg.Listbox(TA, size=(20, 5), enable_events=True, key='-TA-')],
-                                                                        [sg.Text('Nilai:'),sg.Input(tooltip='masukkan nilai 1 s/d 100', default_text='', key='-NILAI-TA-', size=(19,1))],
-                                                                        [sg.Button('Simpan Nilai TA')]], size=(230,155), pad=(0,0))]])] ], pad=(0,0))
-        col4 = sg.Column([[sg.Frame('Actions:',
-                                            [[sg.Column([[sg.Button('Kembali'), sg.Button('Buka File')]],size=(460,45), pad=(0,0))]],size=(505,60))]], pad=(0,0))
-        halaman_lanjut = [[col1],
-                [col2, col3],
-                [col4]]
+        window['-TR-'].update(values=TR)
+        window['-TA-'].update(values=TA)
+
+    elif event == "Cara Penggunaan":
+        sg.popup('Cara Penggunaan',
+                 '1- Pilih file excel terlebih dahulu dengan memilih menu ',
+                 '   "File" lalu tekan "Pilih File"',
+                 '2- Klik "Browse" dan pilih file excel yang akan diakses,',
+                 '   lalu klik "Confirm"',
+                 '3- Jika file berhasil diakses, pilih kelas lalu isi NIM',
+                 '   kemudian pilih jenis tugas dan isi nilainya',
+                 '4- Terakhir klik "Update Nilai TA" atau "Update Nilai TR,"',
+                 '   sesuai jenis tugas yang diisi',
+                 '5- Dan nilai berhasil dimasukkan jika tidak terjadi kesalahan',non_blocking=True)
         
-        window.close()
-        window = sg.Window('Rekap Nilai Kelas '+kelas, halaman_lanjut)
+    elif event == "About":
+        sg.popup('EZRecap',
+                 'Alat bantu asisten lab dalam menginput nilai praktikan',
+                 'dibuat oleh Zaid Immaduddin Abdurrahman',
+                 '',
+                 'contact us :',
+                 'Email : zaidimmaduddin56@gmail.com',
+                 'GitHub : https://github.com/zaid-2002/EZRecap.git')
 
 
 window.close()
